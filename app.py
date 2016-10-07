c5cb777b6abfed4e29e3c9653274b191036832ea
Florian <info@fbrandstetter.eu.org>
2016-10-07 15:03:03 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,12 @@
 #/bin/python
+import cherrypy
+
+
+class HelloWorld(object):
+    @cherrypy.expose
+    def index(self):
+        return "Hello world!"
+
+
+if __name__ == '__main__':
+    cherrypy.quickstart(HelloWorld())
